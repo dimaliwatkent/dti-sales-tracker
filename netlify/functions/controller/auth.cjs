13526ecfb2fde7ad491e2b6fb025df7e199dd4d9
dimaliwatkent <dimaliwatkent@gmail.com>
2024-12-02 22:37:14 +0800

    fix

--- netlify/functions/controller/auth.cjs
+++ netlify/functions/controller/auth.cjs
@@ -1,6 +1,6 @@
 const User = require("../models/user.cjs");
 const bcrypt = require("bcryptjs");
-const jwt = require("cjsonwebtoken");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const {
   generateAccessToken,
